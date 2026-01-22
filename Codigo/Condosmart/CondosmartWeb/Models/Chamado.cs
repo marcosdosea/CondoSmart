1cb461d2c98c0f2b09c27aa32a86cc39e53e58f3
Davi Cardoso <davicardoso524@academico.ufs.br>
2026-01-22 11:49:18 -0300

    Foram feitas os códigos das entidades e os arquivos necessário para o funcionamento do banco de dados e a criação do CondosmartContext Resolved #128

---
+++ Codigo/Condosmart/CondosmartWeb/Models/Chamado.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CondosmartWeb.Models;
+
+public partial class Chamado
+{
+    public int Id { get; set; }
+
+    public string Descricao { get; set; } = null!;
+
+    public DateTime? DataChamado { get; set; }
+
+    public int? MoradorId { get; set; }
+
+    public int? SindicoId { get; set; }
+
+    public int CondominioId { get; set; }
+
+    public string Status { get; set; } = null!;
+
+    public DateTime? CreatedAt { get; set; }
+
+    public virtual Condominio Condominio { get; set; } = null!;
+
+    public virtual Moradore? Morador { get; set; }
+
+    public virtual Sindico? Sindico { get; set; }
+}
---
+++ Codigo/Condosmart/Core/Models/Condominio.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Core.Models;
+
+public partial class Condominio
+{
+    public int Id { get; set; }
+
+    public string Nome { get; set; } = null!;
+
+    public string? Cnpj { get; set; }
+
+    public string? Email { get; set; }
+
+    public string? Telefone { get; set; }
+
+    public string? Rua { get; set; }
+
+    public string? Bairro { get; set; }
+
+    public string? Numero { get; set; }
+
+    public string? Complemento { get; set; }
+
+    public string? Cep { get; set; }
+
+    public string? Cidade { get; set; }
+
+    public string? Uf { get; set; }
+
+    public int? Unidades { get; set; }
+
+    public int? SindicoId { get; set; }
+
+    public DateTime? CreatedAt { get; set; }
+
+    public virtual ICollection<AreaDeLazer> AreaDeLazers { get; set; } = new List<AreaDeLazer>();
+
+    public virtual ICollection<Ata> Ata { get; set; } = new List<Ata>();
+
+    public virtual ICollection<Chamado> Chamados { get; set; } = new List<Chamado>();
+
+    public virtual ICollection<Morador> Moradores { get; set; } = new List<Morador>();
+
+    public virtual ICollection<Pagamento> Pagamentos { get; set; } = new List<Pagamento>();
+
+    public virtual ICollection<Reserva> Reservas { get; set; } = new List<Reserva>();
+
+    public virtual Sindico? Sindico { get; set; }
+
+    public virtual ICollection<UnidadesResidenciais> UnidadesResidenciais { get; set; } = new List<UnidadesResidenciais>();
+}
