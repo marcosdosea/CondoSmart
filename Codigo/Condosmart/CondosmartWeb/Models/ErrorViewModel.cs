233ff191a81e8fef3ad05c56f137a8aabf87a23e
Davi Cardoso <davicardoso524@academico.ufs.br>
2026-01-22 09:39:47 -0300

    Código base Criado com sucesso! Resolved #123

---
+++ Codigo/Condosmart/CondosmartWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Condosmart.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
