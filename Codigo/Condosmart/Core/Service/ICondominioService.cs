904bd7c4f64d6987a74e19624aad2207971a22db
Davi Cardoso <davicardoso524@academico.ufs.br>
2026-01-25 21:31:22 -0300

    Foi Criado a Funcionalidade Manter Condominio, falta apenas os testes !

---
+++ Codigo/Condosmart/Core/Service/ICondominioService.cs
@@ -0,0 +1,13 @@
+﻿using Core.Models;
+
+namespace Core.Service
+{
+    public interface ICondominioService
+    {
+        int Create(Condominio condominio);
+        void Edit(Condominio condominio);
+        void Delete(int id);
+        Condominio GetById(int id);
+        List<Condominio> GetAll();
+    }
+}
