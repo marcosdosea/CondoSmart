49a7ed0a37390ddd230a2f0b288b108283bd38d6
Davi Cardoso <davicardoso524@academico.ufs.br>
2026-01-27 11:18:27 -0300

    Foi Criado o AtaDTO e corrijido o SindicoDTO

---
+++ Codigo/Condosmart/Core/DTO/AtaDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace Core.DTO
+{
+    public class AtaDto
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; } = string.Empty;
+        public DateTime DataReuniao { get; set; }
+
+        public int CondominioId { get; set; }
+        public int? SindicoId { get; set; }
+        public string? Temas { get; set; }
+    }
+}
--- Codigo/Condosmart/Core/DTO/SindicoDTO.cs
+++ Codigo/Condosmart/Core/DTO/SindicoDTO.cs
@@ -1,37 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Core.DTO
+﻿namespace Core.DTO
 {
-    public class SindicoDTO
+    public class SindicoDto
     {
         public int Id { get; set; }
 
-        public string Cpf { get; set; } = null!;
-
-        public string Nome { get; set; } = null!;
-
-        public string? Telefone { get; set; }
+        public string Nome { get; set; } = string.Empty;
 
         public string? Email { get; set; }
 
-        public string? Rua { get; set; }
-
-        public string? Bairro { get; set; }
-
-        public string? Numero { get; set; }
-
-        public string? Complemento { get; set; }
-
-        public string? Cep { get; set; }
+        public string? Telefone { get; set; }
 
         public string? Cidade { get; set; }
 
         public string? Uf { get; set; }
-
-        public DateTime? CreatedAt { get; set; }
     }
 }
