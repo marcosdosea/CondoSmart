b9818585cd0107cd9a52c3580c480219d18ea876
Davi Cardoso <davicardoso524@academico.ufs.br>
2026-01-27 11:13:28 -0300

    Foram criados os *Core/*Service/IAtaService e o *Service/AtaService

---
+++ Codigo/Condosmart/Core/Service/IAtaService.cs
@@ -0,0 +1,13 @@
+﻿using Core.Models;
+
+namespace Core.Service
+{
+    public interface IAtaService
+    {
+        int Create(Ata ata);
+        void Edit(Ata ata);
+        void Delete(int id);
+        Ata? GetById(int id);
+        List<Ata> GetAll();
+    }
+}
