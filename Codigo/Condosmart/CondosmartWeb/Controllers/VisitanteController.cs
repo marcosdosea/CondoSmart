7a16638c11402363ba6f5b9a2c5f54faea73b866
MarcosYago17 <yago17marcos@hotmail.com>
2026-01-27 08:40:59 -0300

    Ulpoad da funcionalidade Visitantes

---
+++ Codigo/Condosmart/CondosmartWeb/Controllers/VisitanteController.cs
@@ -0,0 +1,92 @@
+﻿using AutoMapper;
+using CondosmartWeb.Models;
+using Core.Models;
+using Core.Service;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CondosmartWeb.Controllers
+{
+    public class VisitanteController : Controller
+    {
+        private readonly IVisitanteService _visitanteService;
+        private readonly IMapper _mapper;
+
+        public VisitanteController(IVisitanteService visitanteService, IMapper mapper)
+        {
+            _visitanteService = visitanteService;
+            _mapper = mapper;
+        }
+
+        public ActionResult Index()
+        {
+            var lista = _visitanteService.GetAll();
+            var listaVm = _mapper.Map<List<VisitanteViewModel>>(lista);
+            return View(listaVm);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var item = _visitanteService.GetById(id);
+            if (item == null) return NotFound();
+            var itemVm = _mapper.Map<VisitanteViewModel>(item);
+            return View(itemVm);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(VisitanteViewModel visitanteVm)
+        {
+            if (ModelState.IsValid)
+            {
+                var visitante = _mapper.Map<Visitantes>(visitanteVm);
+                _visitanteService.Create(visitante);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(visitanteVm);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var item = _visitanteService.GetById(id);
+            if (item == null) return NotFound();
+            var itemVm = _mapper.Map<VisitanteViewModel>(item);
+            return View(itemVm);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, VisitanteViewModel visitanteVm)
+        {
+            if (id != visitanteVm.Id) return NotFound();
+
+            if (ModelState.IsValid)
+            {
+                var visitante = _mapper.Map<Visitantes>(visitanteVm);
+                _visitanteService.Edit(visitante);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(visitanteVm);
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var item = _visitanteService.GetById(id);
+            if (item == null) return NotFound();
+            var itemVm = _mapper.Map<VisitanteViewModel>(item);
+            return View(itemVm);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            _visitanteService.Delete(id);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ Codigo/Condosmart/CondosmartWeb/Models/VisitanteViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CondosmartWeb.Models
+{
+    public class VisitanteViewModel
+    {
+        [Display(Name = "Código")]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O campo Nome é obrigatório")]
+        [StringLength(80, ErrorMessage = "O nome não pode exceder 80 caracteres.")]
+        [Display(Name = "Nome")]
+        public string Nome { get; set; } = null!;
+
+        [Required(ErrorMessage = "O CPF é obrigatório")]
+        [StringLength(11, MinimumLength = 11, ErrorMessage = "CPF deve ter 11 caracteres.")]
+        [Display(Name = "CPF")]
+        public string Cpf { get; set; } = null!;
+
+        [StringLength(11, ErrorMessage = "O telefone não pode exceder 11 caracteres.")]
+        [Display(Name = "Telefone")]
+        public string? Telefone { get; set; }
+
+        [StringLength(200, ErrorMessage = "A observação não pode exceder 200 caracteres.")]
+        [Display(Name = "Observação")]
+        public string? Observacao { get; set; }
+
+        [Display(Name = "Data de Entrada")]
+        [Required(ErrorMessage = "A data de entrada é obrigatória")]
+        public DateTime DataHoraEntrada { get; set; } = DateTime.Now;
+
+        [Display(Name = "Data de Saída")]
+        public DateTime? DataHoraSaida { get; set; }
+    }
+}
