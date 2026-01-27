8f1286e6f6142470b49cb67808224ad0b57bd021
João Keweni S R <joaokeweni49@gmail.com>
2026-01-26 21:21:13 -0300

    Criação do Service e da interface de serviços e a view model de Sindico #143

---
+++ Codigo/Condosmart/Core/Service/ISindicoService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Models;
+
+namespace Core.Service
+
+{
+    public interface ISindicoService
+    {
+        int Create(Sindico sindico);
+        void Edit(Sindico sindico);
+        void Delete(int id);
+        Sindico GetById(int id);
+        List<Sindico> GetAll();
+    }
+}
