abd520331b4279c8b67e24021e31afcb59960438
João Keweni S R <joaokeweni49@gmail.com>
2026-01-26 21:22:53 -0300

    Criação do SindicoProfile #143

---
+++ Codigo/Condosmart/CondosmartWeb/Mappers/SindicoProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using CondosmartWeb.Models;
+using Core.Models;
+
+namespace CondosmartWeb.Mappers
+{
+    public class SindicoProfile : Profile
+    {
+        public SindicoProfile()
+        {
+            CreateMap<Sindico, SindicoViewModel>().ReverseMap();
+        }
+    }
+}
