a79533c6f1a64d627c1ebb8b01e6610a8f01c219
Vivian-SI <vivianherdeira@outlook.com>
2026-02-05 22:41:45 -0300

    Criada a funcionalidade da area de lazer. Close #136

--- Codigo/Condosmart/CondosmartWeb/Mappers/ReservaProfile.cs
+++ Codigo/Condosmart/CondosmartWeb/Mappers/ReservaProfile.cs
@@ -8,7 +8,12 @@ namespace CondosmartWeb.Profiles
     {
         public ReservaProfile()
         {
-            CreateMap<Reserva, ReservaViewModel>().ReverseMap();
+            CreateMap<Reserva, ReservaViewModel>()
+                // Ensina o Mapper a pegar o Nome dentro do objeto Area
+                .ForMember(dest => dest.NomeArea, opt => opt.MapFrom(src => src.Area.Nome))
+                // Ensina o Mapper a pegar o Nome dentro do objeto Morador
+                .ForMember(dest => dest.NomeMorador, opt => opt.MapFrom(src => src.Morador.Nome))
+                .ReverseMap();
         }
     }
 }
