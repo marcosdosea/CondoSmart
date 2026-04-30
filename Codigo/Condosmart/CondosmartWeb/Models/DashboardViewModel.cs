ddaa80e94bbc3fe223d47f6978267f5e548340a1
Davi Cardoso <davicardoso524@academico.ufs.br>
2026-04-30 00:33:07 -0300

    Aproxima redesign da referencia visual

--- Codigo/Condosmart/CondosmartWeb/Models/DashboardViewModel.cs
+++ Codigo/Condosmart/CondosmartWeb/Models/DashboardViewModel.cs
@@ -13,5 +13,16 @@ namespace CondosmartWeb.Models
         public int TotalUnidades { get; set; }
         public int ReservasConfirmadas { get; set; }
         public int TotalAreasLazer { get; set; }
+        public IReadOnlyList<DashboardMensalidadeItemViewModel> MensalidadesAbertas { get; set; } = [];
+    }
+
+    public class DashboardMensalidadeItemViewModel
+    {
+        public int Id { get; set; }
+        public string Unidade { get; set; } = "-";
+        public string Morador { get; set; } = "-";
+        public DateTime Vencimento { get; set; }
+        public decimal Valor { get; set; }
+        public string Status { get; set; } = "-";
     }
 }
